35070bca70565271101c7a8fdf897c90e27a2c16
JoWi235 <149091579+JoWi235@users.noreply.github.com>
2024-05-19 08:15:20 +0200

    Add files via upload

---
+++ 1-Web-Site/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('sidebar').classList.toggle('active');
+});
